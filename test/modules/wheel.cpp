aafc2f43fa1fb220bb37a67e47a16c0f2ce9a8a2
Benoit Gilbert <benoitgilbert@outlook.com>
2014-01-10 20:30:05 +0100

    Amelioration de wheel, tentative de fonctionnement.

--- test/modules/wheel.cpp
+++ test/modules/wheel.cpp
@@ -1,19 +1,56 @@
-Wheel::Wheel(btDynamicWorld* world, btRigidBody* robot) {
+#include "wheel.h"
+
+Wheel::Wheel(btDynamicsWorld* world, btRigidBody* robot) : _world(world), _robot(robot) {
 
 }
-
+Wheel::~Wheel() {
+  if(_wheel != NULL) {
+    _world->removeRigidBody(_wheel);
+    delete _wheel->getMotionState();
+    delete _wheel;
+    delete _wheelShape;
+  }
+}
 void Wheel::init() {
 }
-
-void Wheel::init(double x, double y, double z, double d, double l) {
+btVector3 Wheel::getPos() {
+  btTransform trans;
+  _wheel->getMotionState()->getWorldTransform(trans);
+  return trans.getOrigin();  
+}
+void Wheel::init(double x, double y, double z, double r, double l) {
   init();
   
-  btCollisionShape* wheel = new btCylinderShape(btVector3(0.5,1,0.5));
-  btMotionState* wheelMotion = new btDefaultMotionState(btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,3,0)));
+  btCollisionShape* _wheelShape = new btCylinderShapeZ(btVector3(r,l,r));
+  btMotionState* wheelMotion = new
+btDefaultMotionState(btDefaultMotionState(btTransform(btQuaternion(),btVector3(x,y,z))));
   btScalar mass = 1;
   btVector3 wheelInertia(0,0,0);
-  wheel->calculateLocalInertia(mass,wheelInertia);
-  btRigidBody::btRigidBodyConstructionInfo wheelRigidBodyCI(mass,fallMotionState,fallShape,fallInertia);
+  _wheelShape->calculateLocalInertia(mass,wheelInertia);
+  btRigidBody::btRigidBodyConstructionInfo wheelRigidBodyCI(mass,wheelMotion,_wheelShape,wheelInertia);
   _wheel = new btRigidBody(wheelRigidBodyCI);
+	_wheel->setFriction(1.);
+	_wheel->setRollingFriction(0.3);
+  _world->addRigidBody(_wheel);
 
+}
+
+double Wheel::getAngle() {
+  btTransform trans;
+  _wheel->getMotionState()->getWorldTransform(trans);
+  btQuaternion q = trans.getRotation();
+  //return q.getAngle();  
+  btScalar z,y,x;
+  trans.getBasis().getEulerYPR(x,y,z);
+  return x;
+  
+  //return trans.getRotation().angle(btQuaternion(0,0,1,1));  
+}
+
+double Wheel::getTorque() {
+  return _wheel->getTotalTorque().getZ();
+}
+
+void Wheel::setTorque(double f) {
+  _wheel->applyTorque(btVector3(0,0,f));
 }
