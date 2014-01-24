56f537dd69f7dff3c9f7779f22df5a596f03ecf4
hugo <hbrunie0@gmail.com>
2014-01-24 16:01:42 +0100

    ajout du coordinateur

---
+++ src/coordinator/coordinator.hpp
@@ -0,0 +1,35 @@
+#include "../physical_calculator/physical_calculator.hpp"
+#include <QObject>
+#include <QMap>
+#include <QString>
+#include <QProcess>
+class Coordinator: public QObject{
+Q_OBJECT
+
+signals:
+  void calcNextStep(); 
+  void log(QString); 
+  void timer(float);
+public slots:
+  //! \brief read the message received from the Client Thread
+  void CTReceived();
+  //! \brief
+  void openTable(QString XMLPath);
+  void play();
+  void pause();
+  void stepDone();
+  void openRobot(QString XMLPath, Slot slot);
+public:
+  enum Slot{MAIN_ROBOT1,SECOND_ROBOT1,MAIN_ROBOT2,SECOND_ROBOT2} ;
+private:
+  QMap<QString robot_name, QMap<QString module_name, Modules* modules>>_modules;  
+  QMap<QString robot_code,QProcess* robot_process>;
+
+  PysicalCalculator _physic;
+  
+  void sendDeviceMessage(QString name, QString msg, QProcess* p);
+  void sendMessage(QString msg, QProcess* p);
+  void sendSyncMessage();
+
+};
+
