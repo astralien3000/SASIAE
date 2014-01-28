0b5d3b30b47c0169151da3e7796d05ee8135b0d2
Benoit Gilbert <benoitgilbert@outlook.com>
2014-01-28 12:11:24 +0100

    Avancement du coordinateur

--- src/coordinator/coordinator.cpp
+++ src/coordinator/coordinator.cpp
@@ -1,26 +1,39 @@
 #include "coordinator.hpp"
-/*TODO*/
-void Coordinator::CTReceived(){
-  while()
+
+
+void Coordinator::CTReceived() {
 }
-void Coordinator::openTable(QString XMLPath){
+
+
+void Coordinator::play() {
+  _running = true;
+  emit(calcNextStep());
+}
+
+void Coordinator::pause() {
+  _running = false;
+}
+
+void Coordinator::stepDone() {
+  
+}
+
+void Coordinator::openTable(const QString& XMLPath) {
 
 }
-void Coordinator::play(){
-}
-void Coordinator::pause(){
 
-}
-void Coordinator::openRobot(QString XMLPath, Slot slot){
+void Coordinator::openRobot(const QString& XMLPath, Coordinator::Slot slot) {
 
 }
 
 Coordinator::Coordinator() {
-
+  _running = false;
 }
+
 Coordinator& Coordinator::getInstance() {
   if(_instance)
     return *_instance;
   else
     return *(_instance = new Coordinator()); 
 }
+
--- src/coordinator/coordinator.hpp
+++ src/coordinator/coordinator.hpp
@@ -5,38 +5,52 @@
 #include <QProcess>
 
 class Coordinator: public QObject{
-Q_OBJECT
+  Q_OBJECT
 
-signals:
-  void calcNextStep(); 
-  void log(QString); 
-  void timer(float);
-public slots:
-  //! \brief coordinator is unique, so we need to get is instance (and create one if it doesn't existe)
-  static Coordinator& getInstance();
-  //! \brief read the message received from the Client Thread
-  void CTReceived();
-  //! \brief
-  void openTable(QString XMLPath);
-  void play();
-  void pause();
-  void stepDone();
-  void openRobot(QString XMLPath, Slot slot);
-public:
-  enum Slot{MAIN_ROBOT1,SECOND_ROBOT1,MAIN_ROBOT2,SECOND_ROBOT2};
-  ~Coordinator();
+  signals:
+    void calcNextStep(); 
+    void log(QString); 
+    //! \brief time for UI sync in ms
+    void timer(int);
 
-private:
-  QMap<QString robot_name, QMap<QString module_name, Modules* modules>>_modules;  
-  QMap<QString robot_code,QProcess* robot_process>;
+  public:
+    enum Slot{MAIN_ROBOT1,SECOND_ROBOT1,MAIN_ROBOT2,SECOND_ROBOT2};
+    ~Coordinator();
 
-  PysicalCalculator _physic;
-  
-  void sendDeviceMessage(QString name, QString msg, QProcess* p);
-  void sendMessage(QString msg, QProcess* p);
-  void sendSyncMessage();
-  Coordinator();
-  
-  static Coordinator* _instance;
+  public slots:
+
+    //! \brief coordinator is unique, so we need to get is instance (and create one if it doesn't existe)
+    static Coordinator& getInstance();
+
+    //! \brief
+    void play();
+    void pause();
+    void stepDone();
+    void openTable(QString XMLPath);
+    void openRobot(QString XMLPath, Slot slot);
+
+  private slots:
+    //! \brief read the message received from the Client Thread
+    void CTReceived(QString);
+    //! \brief read the message received from the Modules
+    void MReceived(QString);
+
+
+  private:
+    QMap<QString/*robot_name*/, 
+          QMap<QString/*module_name*/, 
+                Modules* /*modules*/>> _modules;
+    QMap<QString/*robot_code*/,
+          QProcess* /*robot_process*/> _codes;
+
+    PysicalCalculator _physic;
+    
+    void sendDeviceMessage(QString name, QString msg, QProcess* p);
+    void sendMessage(QString msg, QProcess* p);
+    void sendSyncMessage();
+    Coordinator();
+    
+    static Coordinator* _instance;
+    bool running;
 };
 
