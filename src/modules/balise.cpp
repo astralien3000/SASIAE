6c9e40c7bcbfaebf70a14126c9819755ec59cbc5
Sarcarx <sarcarx@localhost.localdomain>
2014-02-01 18:46:13 +0100

    modif de la balise

--- src/modules/balise.cpp
+++ src/modules/balise.cpp
@@ -5,11 +5,12 @@
 
 //cylindre de masse 0.1
 
-Balise::Balise(Robot robot, QObject* parent) {
+Balise::Balise(Robot *robot, QObject* parent): Modules(parent) {
+    btTransform trans;
 	trans = robot->getChassisWorldTransform();
 	btScalar z,y,x;
 	trans.getBasis().getEulerZYX(z,y,x);
-	this.position =
+	this->position =
 	btVector3(trans.getOrigin().getX()
 		, trans.getOrigin().getY()
 		, trans.getOrigin().getZ());
--- src/modules/balise.hpp
+++ src/modules/balise.hpp
@@ -1,14 +1,15 @@
 #include "modules.hpp"
+#include "../PhysicalCalculator/robot.hpp"
 #include <QObject>
 #include <QString>
 #include <QStandardItem>
-#include <btVector3.h>
+#include <btBulletDynamicsCommon.h>
 
 class Balise : public Modules {
 Q_OBJECT
 
 public:
-  Balise(Robot robot, QObject* parent);
+  Balise(Robot* robot, QObject* parent);
 
 public slots:
 
