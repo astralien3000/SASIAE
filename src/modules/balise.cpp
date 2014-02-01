9fdd52ebca4a817e807292ff9991a082b4e577fc
Sarcarx <sarcarx@localhost.localdomain>
2014-02-01 19:16:17 +0100

    creation des balises est complète

--- src/modules/balise.cpp
+++ src/modules/balise.cpp
@@ -6,14 +6,34 @@
 //cylindre de masse 0.1
 
 Balise::Balise(Robot *robot, QObject* parent): Modules(parent) {
-    btTransform trans;
+	btDiscreteDynamicsWorld * myscene=getScene();
+
+
+	btTransform trans;
 	trans = robot->getChassisWorldTransform();
 	btScalar z,y,x;
 	trans.getBasis().getEulerZYX(z,y,x);
 	this->position =
 	btVector3(trans.getOrigin().getX()
 		, trans.getOrigin().getY()
-		, trans.getOrigin().getZ());
+		, trans.getOrigin().getZ()); //la position de la balise.
+
+	//masse très faible pour ne pas empecher les mouvements du robot.
+	btScalar mass(0.1);
+
+	btDefaultMotionState* myMotionState = new btDefaultMotionState(trans);
+	// Collision avec rien
+	btCollisionShape* boxShape = new btCylinderShape(btVector3(3,0,0));
+
+	btVector3 localInertia(0,0,0);
+	boxShape->calculateLocalInertia(mass, localInertia);
+
+
+
+	btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,boxShape,localInertia);
+	btRigidBody* boxBody=new btRigidBody(rbInfo);
+
+	myscene->addRigidBody(boxBody);
 }
 
 
--- src/modules/balise.hpp
+++ src/modules/balise.hpp
@@ -1,9 +1,12 @@
 #include "modules.hpp"
 #include "../PhysicalCalculator/robot.hpp"
+#include "../PhysicalCalculator/physical_calculator.hpp"
+
 #include <QObject>
 #include <QString>
 #include <QStandardItem>
 #include <btBulletDynamicsCommon.h>
+
 
 class Balise : public Modules {
 Q_OBJECT
