93e3287983e3314cf66a1708df09d286724a6950
Sarcarx <sarcarx@localhost.localdomain>
2014-02-10 10:10:29 +0100

    correction de la mise a jour d'une balise et des mises à jour de dataRoot dans balise et servo

--- src/modules/servo.cpp
+++ src/modules/servo.cpp
@@ -30,8 +30,7 @@ void Servo::received(QString message) {
 	if(list.at(0) == "value") {
   // le message est du type : "value [position]"
 		_position = list.at(1).toInt();
-		_dataRoot->removeRow(1);
-		_dataRoot->appendRow(QList<QStandardItem*>() << new QStandardItem("Position") << new QStandardItem(QString()+ _position));
+		_dataRoot->child(1,1)->setText(QString() + _position);
 	}
 }
 
