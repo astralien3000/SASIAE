2502b0a952ae4a015819597f5eba7fa4d5eddbd3
Benoit Gilbert <benoitgilbert@outlook.com>
2014-02-17 11:11:39 +0100

    Correction du positionnement de départ du capteur de distance

--- src/physical_calculator/dist_sensor.cpp
+++ src/physical_calculator/dist_sensor.cpp
@@ -1,16 +1,18 @@
 #include "dist_sensor.hpp"
 
-DistSensor::DistSensor(btDynamicsWorld* world, btRigidBody* chassis) : _world(world), _chassis(chassis) {}
+DistSensor::DistSensor(btDynamicsWorld* world, btRigidBody* chassis) : _chassis(chassis), _world(world) {}
 
-DistSensor::DistSensor(btDynamicsWorld* world, btRigidBody* chassis, const btVector3 &pos, const btVector3 &direction, const btVector3 &boxSize, const btScalar &mass) : _world(world), _chassis(chassis) {
+DistSensor::DistSensor(btDynamicsWorld* world, btRigidBody* chassis, const btVector3 &pos, const btVector3 &direction, const btVector3 &boxSize, const btScalar &mass) : _chassis(chassis), _world(world)  {
   init(pos,direction,boxSize,mass);  
 }
+
 void DistSensor::init(const btVector3 &pos, const btVector3 &direction, const btVector3 &boxSize, const btScalar &mass){
+  _dir = direction;
  //NOTE: boxsize vu de face, X profondeur, Y hauteur, Z largeur
  //NOTE: boxsize en halfextents i.e. dimension d'un quart de boite
  //creation de la boite
  btCollisionShape* boxShape = new btBoxShape(boxSize);
- btDefaultMotionState* boxMotionState = new btDefaultMotionState(btTransform());
+ btDefaultMotionState* boxMotionState = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,0,0)));
  btVector3 boxInertial(0,0,0);
  boxShape->calculateLocalInertia(mass, boxInertial);
  btRigidBody::btRigidBodyConstructionInfo boxBodyCi(mass, boxMotionState, boxShape, boxInertial);
@@ -21,12 +23,12 @@ void DistSensor::init(const btVector3 &pos, const btVector3 &direction, const bt
  btVector3 normal = direction;
  normal.normalize();
  btVector3 relativeVec(-1*normal.getZ(), normal.getY(), normal.getX());
- btVector3 pivotInChassis1 = pos + relativeVec*btScalar(boxSize.getX()*4/3); 
- btVector3 pivotInChassis2 = pos + relativeVec*btScalar(-1*boxSize.getX()*4/3); 
- btVector3 pivotInBox1(boxSize.getX()*btScalar(4/3),0,boxSize.getZ());
- btVector3 pivotInBox2(boxSize.getX()*btScalar(-4/3),0,boxSize.getZ());
+ btVector3 pivotInChassis1 = pos + relativeVec*btScalar(boxSize.getX()*4./3.); 
+ btVector3 pivotInChassis2 = pos + relativeVec*btScalar(boxSize.getX()*-4./3.); 
+ btVector3 pivotInBox1(boxSize.getX()*btScalar(-4./3.),0,-boxSize.getZ());
+ btVector3 pivotInBox2(boxSize.getX()*btScalar(4./3.),0,-boxSize.getZ());
  btTypedConstraint* p2pL = new btPoint2PointConstraint(*_chassis,*_sensor_box, pivotInChassis1,pivotInBox1);
- btTypedConstraint* p2pR = new btPoint2PointConstraint(*_chassis,*_sensor_box, pivotInChassis1,pivotInBox2);
+ btTypedConstraint* p2pR = new btPoint2PointConstraint(*_chassis,*_sensor_box, pivotInChassis2,pivotInBox2);
  
  // add constraint to world
  _world->addConstraint(p2pL);
@@ -41,8 +43,10 @@ double DistSensor::getDist(){
   return getCachedDist();
 }
 void DistSensor::calcDist(){
- btVector3 start(0,0,0);//TODO find the right vect 
- btVector3 end(0,0,0);//TODO find the right vect 
+  btTransform trans;
+ _sensor_box->getMotionState()->getWorldTransform(trans);//TODO check if it's the right vect 
+ btVector3 start = trans.getOrigin();
+ btVector3 end = start;//TODO check if it's the right vect 
  btCollisionWorld::ClosestRayResultCallback rayCallback(start,end);
  //rayCallback.m_flags |= btTriangleRaycastCallback::kF_FilterBackfaces;
  _world->rayTest(start,end,rayCallback);
--- src/physical_calculator/dist_sensor.hpp
+++ src/physical_calculator/dist_sensor.hpp
@@ -14,6 +14,7 @@ public:
 private:
   btRigidBody* _chassis;
   btRigidBody* _sensor_box;
+  btVector3 _dir;
   double _cache;
   btDynamicsWorld* _world;
 
