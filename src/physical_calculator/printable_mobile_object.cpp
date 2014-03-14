eacfc86b5063063c8d4d16e04a0aef047ad84f63
hugo <hbrunie0@gmail.com>
2014-03-14 11:26:49 +0100

    ajout de la classe PositionData qui fait la liaison entre les données bullet et données Qt et modi du PMO

--- src/physical_calculator/printable_mobile_object.cpp
+++ src/physical_calculator/printable_mobile_object.cpp
@@ -1,14 +1,27 @@
 #include "printable_mobile_object.hpp"
 
-PrintableMobileObject::PrintableMobileObject(QString name, btRigidBody *body) : _name(name), _pos(new struct position_data),_body(body){}
+PrintableMobileObject::PrintableMobileObject(QString name, btRigidBody *body) :
+    _name(name) /*_pos(new struct position_data)*/,_body(body)
+{
+    _pos=new PositionData();
+}
 
-PrintableMobileObject::PrintableMobileObject(btRigidBody *body): _name("unknown"), _pos(new struct position_data),_body(body){}
+PrintableMobileObject::PrintableMobileObject(btRigidBody *body):
+    _name("unknown") /*_pos(new struct position_data)*/,_body(body)
+{
+    _pos=new PositionData();
+}
 
 
-struct position_data * PrintableMobileObject::getPosition() {
+PositionData *PrintableMobileObject::getPosition() {
   	btTransform trans;
   	_body->getMotionState()->getWorldTransform(trans);
-  	_pos->position=trans.getOrigin();
-  	_pos->angle=trans.getRotation();
-	return _pos;
+    _pos->setPosition(trans.getOrigin());
+    _pos->setRotation(trans.getRotation());
+
+    /* use of structure :
+     * _pos->position=trans.getOrigin();
+     * _pos->angle=trans.getRotation();
+    */
+return _pos;
 }
--- src/physical_calculator/printable_mobile_object.hpp
+++ src/physical_calculator/printable_mobile_object.hpp
@@ -4,26 +4,27 @@
 #include <btBulletDynamicsCommon.h>
 #include <QString>
 #include <QVector>
-// #include Mesh
-//used as a list in the gui coordinateur to be printed on Qt
 
-struct position_data {
+#include "position_data.hpp"
+
+/*struct position_data {
   btVector3 position; // TODO : transform it to QVector<int>
   //QVector<int> position;
  //vector de position x y z
   btQuaternion angle;
-};
+};*/
 
 class PrintableMobileObject /*: public Mesh*/ {
 private:
   QString _name;
-  struct position_data * _pos;
+  //struct position_data * _pos;
+    PositionData * _pos;
 protected:
   btRigidBody *_body;
 public:
   PrintableMobileObject(QString name, btRigidBody *body);
   PrintableMobileObject(btRigidBody *body);
-  position_data * getPosition(); //renvoie 4 entiers dans une structure (un vecteur 3 dimension et un angle) au lieu du btVector3 : x y z orientation
+  PositionData * getPosition(); //renvoie 4 entiers dans une structure (un vecteur 3 dimension et un angle) au lieu du btVector3 : x y z orientation
 };
 
 #endif
