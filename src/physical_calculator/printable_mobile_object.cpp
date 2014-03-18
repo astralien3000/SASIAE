0af9ac88857d9915f8bbb4122b0303a009da1fb4
Your Name <you@example.com>
2014-03-18 17:57:33 +0100

    update du moyen de fonctionnement de gui

--- src/physical_calculator/printable_mobile_object.cpp
+++ src/physical_calculator/printable_mobile_object.cpp
@@ -1,6 +1,7 @@
 #include "printable_mobile_object.hpp"
 
 QVector<const PrintableMobileObject*> PrintableMobileObject::objects;
+QMap<QString,QPair<QPixmap*,int>*> PrintableMobileObject::images;
 
 
   const QVector<const PrintableMobileObject*>& PrintableMobileObject::getObjectsList() {
@@ -13,8 +14,24 @@ PrintableMobileObject::PrintableMobileObject(QString name, btRigidBody *body) :
 {
     _pos=new PositionData();
     objects.append(this);
+	//si image est dans tableau, ++, sinon ajouter image dans tableau.
+    auto it = images.find(name);
+    if(it!=images.end()) {//found
+    	(*it)->second++;
+    } else { //add the pixmap in the map !
+        images.insert(name,new QPair<QPixmap*,int>(new QPixmap(""+name),0));
+    }
+
 }
 PrintableMobileObject::~PrintableMobileObject() {
+    auto it = images.find(this->_name);
+    if(it!=images.end()) {//found
+    	(*it)->second--;
+        if((*it)->second==0) {
+    		QPair<QPixmap*,int>* tobedeleted = (images.take(this->_name));
+    		delete(tobedeleted);
+    	}
+    }
 	int objectPosition = objects.indexOf(this);
 	objects.remove(objectPosition);
 }
--- src/physical_calculator/printable_mobile_object.hpp
+++ src/physical_calculator/printable_mobile_object.hpp
@@ -4,8 +4,15 @@
 #include <btBulletDynamicsCommon.h>
 #include <QString>
 #include <QVector>
+#include <QPair>
+#include <QPixmap>
+#include <QMap>
 
 #include "position_data.hpp"
+
+//PMO contient un pointeur sur QGraphicsItem, le coordinateur doit appeler addPMO(scene) à chaque création de PMO (à coderdans gui coordinator)
+//reset PMO dans gui coordinator doit etre implémentée pour virer tous les PMO déjà mis, dans update de gui coord, il itère et appelle set_position sur chaque objet, puis table.update pour rafraichir.
+
 
 /*
 Alternative
@@ -19,18 +26,20 @@ struct PositionData {
 class PrintableMobileObject /*: public Mesh*/ {
 private:
 	static QVector<const PrintableMobileObject*> objects;
+	static QMap<QString,QPair<QPixmap*,int>*> images; //changer QPixmap en Qgraphic Item ou mettre une liste PMO/QgraphicItem dans guiCoordinator
 //destructeur -> enlever les objets qui n'existent plus.
 private:
-  QString _name;
+	QString _name; //nom de l'image inclue pour le Pixmap !!!!!! 
   //struct position_data * _pos;
-    PositionData * _pos;
+	PositionData * _pos;
+
 protected:
 	btRigidBody *_body;
 public:
-  PrintableMobileObject(QString name, btRigidBody *body);
-  ~PrintableMobileObject();
-  static const QVector<const PrintableMobileObject*>& getObjectsList();
-  PrintableMobileObject(btRigidBody *body);
+	PrintableMobileObject(QString name, btRigidBody *body);
+	~PrintableMobileObject();
+	static const QVector<const PrintableMobileObject*>& getObjectsList();
+	PrintableMobileObject(btRigidBody *body);
   const PositionData * getPosition() const; //renvoie 4 entiers dans une structure (un vecteur 3 dimension et un angle) au lieu du btVector3 : x y z orientation
 };
 
