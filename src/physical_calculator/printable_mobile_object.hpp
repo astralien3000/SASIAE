a959e93f21f454fdaa3708eeb44197270f1f36bc
Sarcarx <blopblipblop@example.com>
2014-03-21 12:55:47 +0100

    ajout de la connexion et du morceau de rapport pour nicolas

--- src/physical_calculator/printable_mobile_object.hpp
+++ src/physical_calculator/printable_mobile_object.hpp
@@ -10,35 +10,37 @@
 #include <QGraphicsPixmapItem>
 #include "position_data.hpp"
 #include "stlmesh.hpp"
+#include "../coordinator/gui_coordinator.hpp"
 
 /*PMO contient un pointeur sur QGraphicsPixmapItem, le coordinateur doit appeler addPMO(scene) 
 à chaque création de PMO (à coderdans gui coordinator)
 reset PMO dans gui coordinator doit etre implémentée pour virer tous les PMO déjà mis, 
 dans update de gui coord, il itère et appelle update sur chaque PMO qui lui meme fait un 
-truc du style (graphicsitem->setposition) objet, puis !!! PEUT ETRE !!! table.update pour rafraichir.
+truc du style (graphicsitem->setposition) objet, puis !!! PEUT ETRE(si nécessaire) !!! table.update pour rafraichir.
 */
 
 class PrintableMobileObject : public STLMesh {
+signals:
+    void updateTable(); //si on veut faire un truc génial on pourra utiliser ce signal pendant la cration du PMO pour le rajouter directement à la table !! 
 private:
-  static const QString _img_path;
+	static const QString _img_path;
 	static QVector<PrintableMobileObject*> objects;
 	static QMap<QString,QPair<QPixmap*,int>*> images; //changer QPixmap en Qgraphic Item ou mettre une liste PMO/QgraphicItem dans guiCoordinator
 //destructeur -> enlever les objets qui n'existent plus.
 private:
-	QString _name; //nom de l'image inclue pour le Pixmap !!!!!! 
+	QString _name; //nom de l'image inclue pour le Pixmap 
 	QGraphicsPixmapItem* _item; //permet d'update l'affichage.
   // struct position_data * _pos;contenu dans le Mesh mtn
 public:
-  PrintableMobileObject(const QString name, const STLMesh & mesh);
-//  PrintableMobileObject(const QString path, float mass, PositionData start_pos, const QString name);
-  PrintableMobileObject(const QString path, float mass, PositionData start_pos, const QString name);
-  ~PrintableMobileObject();
-  static const QVector<PrintableMobileObject*>& getObjectsList();
-  PrintableMobileObject(const STLMesh& mesh);
-  void update();
-  QGraphicsPixmapItem* getItem();
-  // Déplacé dans STLMesh const PositionData & getPosition() const; //renvoie 4 entiers dans une structure (un vecteur 3 dimension et un angle) au lieu du btVector3 : x y z orientation
-  
+	PrintableMobileObject(const QString name, const STLMesh & mesh);
+//  PrintableMobileObject(const QString path, float mass, PositionData start_pos, const QString name); peut être remise si besoin
+	PrintableMobileObject(const QString path, float mass, PositionData start_pos, const QString name);
+	~PrintableMobileObject();
+	static const QVector<PrintableMobileObject*>& getObjectsList();
+	PrintableMobileObject(const STLMesh& mesh);
+	void update();
+	QGraphicsPixmapItem* getItem();
+  // Déplacé dans STLMesh const PositionData & getPosition() const; 
 
 };
 
